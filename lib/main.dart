024714f20ffcac78548912bb98538aac6944a912
dharmishta basita <dharmishtab@gmail.com>
2020-02-17 06:02:07 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey[300],
+        title: Text('I Am Poor'),
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.lightBlue[200],
+      body: Center(
+        child: Image(
+          image: AssetImage('images/rock.png'),
+        ),
+      ),
+    ),
+  ));
+}
